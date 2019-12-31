d1ccd71fc47bd43ad87e6fb3efa0fd28e56deb1d
dlukianenko <dlukianenko@luxoft.com>
2019-12-31 17:28:33 +0200

    added fun get_list_projects

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-import requests, json
+import requests
+import json
+from lxml import html, etree
+from collections import namedtuple
 
-url = 'https://freelancehunt.com/my'
 
-json_file = open('data.json')
-json_str = json_file.read()
-json_data = json.loads(json_str)
-cookies = dict()
-for e in json_data:
-    cookies[e['name']] = e['value']
+def get_with_cookies(url, json_file=None):
+    '''Функция возвращает html-разметку у запрашиваемого сайта, используя cookies'''
+    cookies = dict()
+    if(json_file == None):
+        json_file = open(json_file)
+        json_str = json_file.read()
+        json_data = json.loads(json_str)
 
-s = requests.Session()
-r = s.get(url, cookies=cookies)
-print(r.text[:200])
+        for e in json_data:
+            cookies[e['name']] = e['value']
+
+    s = requests.Session()
+    return s.get(url, cookies=cookies)
+
+
+def get_list_projects(response):
+    '''Функция возвращает список проектов из предоставленой html-разметки'''
+    tree = html.fromstring(response.text)
+
+    names = []
+    for name in tree.xpath('//a[contains(concat(" ",normalize-space(@class)," ")," visitable ")]'):
+        names.append(name.text)
+
+    links = []
+    for link in tree.xpath('//a[contains(concat(" ",normalize-space(@class)," ")," visitable ")]/@href'):
+        links.append(link)
+
+    i = 0
+    projects = []
+    while(i < len(names)):
+        projects.append({
+            'name': names[i],
+            'link': links[i],
+        })
+        i += 1
+    return projects
+
+
+def main():
+    namefile_json = 'data'
+    response = get_with_cookies('https://freelancehunt.com/projects?skills%5B%5D=124', namefile_json)
+    projects = get_list_projects(response)
+
+    for el in projects:
+        print(el['link'])
+
+if __name__ == "__main__":
+    main()
